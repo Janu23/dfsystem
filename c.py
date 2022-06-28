5df5c42e7d664f0ecc1c4440eddf502a62250841
Thiago Rodrigues <53875918+thiagorss@users.noreply.github.com>
2022-06-28 20:52:48 -0300

    Update c.py
    correções

--- c.py
+++ c.py
@@ -2,18 +2,16 @@ import socket
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-client.connect(('localhost', 10000))
+client.connect(('localhost', 10002))
 print('conectado')
 
 namefile=str(input('nome: '))
 client.send(namefile.encode())
 
-print("nao travou")
 with open (namefile, 'wb')as file:
     while 1:
         data = client.recv(1000000)
         if not data:
             break
         file.write(data)
-
 
